392c812eea4336a465bb50e2e67dde400ddb39ff
Dhananjoy <deydhananjoy5@gmail.com>
2023-08-13 12:51:06 +0530

    added custom icons

---
+++ src/IconShapeRhombus.js
@@ -0,0 +1,17 @@
+// icon:shape-rhombus | CSS Icons https://css.gg/ | Astrit
+import * as React from "react";
+
+function IconShapeRhombus(props) {
+  return (
+    <svg fill="none" viewBox="0 0 24 24" height="1em" width="1em" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        d="M12 6.343L6.343 12 12 17.657 17.657 12 12 6.343zM2.1 12l9.9 9.9 9.9-9.9L12 2.1 2.1 12z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default IconShapeRhombus;
--- src/PropertyList.js
+++ src/PropertyList.js
@@ -1,31 +1,45 @@
 import React from 'react'
-
-
+import IconBxBed from './IconBxBed';
+import IconShapeRhombus from './IconShapeRhombus';
+import IconBxsBath from './IconBxsBath';
 const PropertyListing = ({ properties }) => {
     return (
       <div className="grid grid-cols-3 gap-4 ml-5">
         {properties.map((property, index) => (
           <div className="property-item" key={index}>
             
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow white:bg-gray-800 white:border-gray-700">
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow white:bg-gray-800 white:border-gray-700">
 
-    < p   >
-        <img class="rounded-t-lg" src={property.image_url} alt={property.property_name} />
-    </ p>
-    <div class="p-5">
-        < p   >
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{property.property_name}</h5>
-        </ p>
-        
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Address: {property.address}        </p>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{property.number_of_beds} beds           {property.number_of_bathrooms} bathrooms {property.area} sq ft</p>
-        < p    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Popular
-             {/* <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg> */}
-        </ p>
+                <p>
+                    <img class="rounded-t-lg" src={property.image_url} alt={property.property_name} />
+                </ p>
+                <div class="p-5">
+            <p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{property.property_name}</h5>
+            </ p>
+            <div class="flex items-center justify-between">
+            <p class="text-3xl font-bold text-gray-900 dark:text-black">${property.rent_per_month}</p>            
+
     </div>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Address: {property.address}        </p>
+            <div className='flex mb-3'>
+                <IconBxBed/>            
+                <div class="  mr-2 font-normal text-gray-700 dark:text-gray-400 ">{property.number_of_beds} beds</div>
+                <IconBxsBath/>
+                <div class=" mr-2 ml-2 font-normal text-gray-700 dark:text-gray-400">{property.number_of_bathrooms} bathrooms </div>
+                <IconShapeRhombus/>
+                <div class="ml-2 mr-2 font-normal text-gray-700 dark:text-gray-400">{property.area} sq ft</div>
+            </div>
+
+            < p    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Popular
+                {/* <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg> */}
+            </ p>
+            
+    </div>
+    
 </div>
 
 
